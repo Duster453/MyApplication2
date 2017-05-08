de288b3f252090d957ce271ac97dc840b569b298
Kushal <kushalgupta0565@gmail.com>
2017-05-09 04:52:08 +0530

    Initial commit 1

---
+++ app/src/main/java/com/testangle/mobileprogramming/myapplication2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.testangle.mobileprogramming.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
+}
